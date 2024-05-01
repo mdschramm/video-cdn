47e8fd63c3fd9e5c4d347251b62a054325f07ca7
mdschramm <mark@air.inc>
2024-05-01 17:12:33 -0400

    basic client and server video player with cv2

---
+++ socket_client.py
@@ -0,0 +1,72 @@
+import threading
+import socketserver
+import socket
+import time
+import cv2
+import struct
+import pickle
+import numpy as np
+
+address = ('localhost', 10001)
+
+
+# client
+def connect2server():
+    s = socket.create_connection(address)
+    # collects bytes from the server in chunks
+    window = b""
+    # size of data containing the frame size
+    frame_size_header_len = struct.calcsize("Q")
+    while True:
+        # Obtain next frame's size
+        while len(window) < frame_size_header_len:
+            packet = s.recv(4 * 1024) 
+            if not packet:
+                break
+            window += packet
+        if not window:
+            break
+
+        # unpack the frame size
+        frame_size_data = window[:frame_size_header_len]
+        frame_size = struct.unpack("Q", frame_size_data)[0]
+
+        # discard frame size data from window
+        window = window[frame_size_header_len:]
+
+        # obtain frame size worth of data
+        while len(window) < frame_size:
+            window += s.recv(4 * 1024)  # 4K buffer size
+
+        # decode and render frame
+        frame_data = window[:frame_size]
+        image = cv2.imdecode(
+            np.frombuffer(frame_data, dtype=np.uint8),
+            cv2.IMREAD_COLOR
+        )
+        cv2.imshow('Client', image)
+        if cv2.waitKey(1) == 13:
+            break
+
+        # discard last frame's data and continue
+        window = window[frame_size:]
+    cv2.destroyAllWindows()
+
+
+    s.close()
+
+
+
+if __name__ == '__main__':
+    connect2server()
+    # threadlist = []
+    # threadcount = 1
+
+    # for i in range(threadcount):
+    #     print('making client', i)
+    #     client = threading.Thread(target=connect2server)
+    #     threadlist.append(client)
+    #     client.start()
+
+    # for i in range(threadcount):
+    #     threadlist[i].join()
---
+++ socket_server.py
@@ -0,0 +1,53 @@
+import threading
+import socketserver
+import cv2
+import socket
+import time
+import struct
+import numpy as np
+import pickle
+
+
+class ThreadedEchoRequestHandler(
+        socketserver.BaseRequestHandler,
+):
+
+    def handle(self):
+        cur_thread = threading.current_thread()
+        print('Server Thread: %s' % cur_thread.name)
+        # video = '3698047-uhd_3840_2160_15fps.mp4'
+        video = 'soccer_stadium.mp4'
+        cap = cv2.VideoCapture(video)
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            ret, jpeg = cv2.imencode('.jpg', frame, [480, 480])
+            frame_data = jpeg.tobytes()
+            self.request.sendall(struct.pack("Q", len(frame_data)))
+            self.request.sendall(frame_data)
+            if cv2.waitKey(1) == 13:
+                break
+        cap.release()
+       
+
+class ThreadingServer(socketserver.ThreadingTCPServer):
+
+    def __init__(self, server_address, RequestHandlerClass):
+        socketserver.ThreadingTCPServer.__init__(self, server_address, RequestHandlerClass)
+
+
+if __name__ == '__main__':
+
+    address = ('localhost', 10001)
+    server = ThreadingServer(address,
+                                ThreadedEchoRequestHandler)
+    ip, port = server.server_address  # what port was assigned?
+
+    try:
+        print('Server loop running:')
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.shutdown()
+        server.socket.close()
+
